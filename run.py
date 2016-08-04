56b8a59814f10ffa0043a89457a6b5fe0bb8f24b
niusmallnan <zhangzb@neunn.com>
2016-08-04 03:55:31 +0000

    monitoring instance create failed

--- run.py
+++ run.py
@@ -34,7 +34,8 @@ def register_args():
     arg_cattle_acckey = env('CATTLE_ACCESS_KEY')
     arg_cattle_seckey = env('CATTLE_SECRET_KEY')
     arg_timer = env('TIMER', default=60) #seconds
-    arg_timeout = env('TIMEOUT', default=60*1000*10)
+    arg_service_timeout = env('SERVICE_TIMEOUT', default=60*1000*10)
+    arg_instance_start_count = env('INSTANCE_START_COUNT', default=5)
 
     parser = argparse.ArgumentParser(add_help=help)
     parser.add_argument('--cattle-url', dest='cattle_url',
@@ -44,8 +45,10 @@ def register_args():
     parser.add_argument('--cattle-secret-key', dest='cattle_secret_key',
                         default=arg_cattle_seckey)
     parser.add_argument('--timer', dest='timer', type=int, default=arg_timer)
-    parser.add_argument('--timeout', dest='timeout', type=int,
-                        default=arg_timeout)
+    parser.add_argument('--service-timeout', dest='service_timeout', type=int,
+                        default=arg_service_timeout)
+    parser.add_argument('--instance-start-count', dest='instance_start_count',
+                        type=int, default=arg_instance_start_count)
     args = parser.parse_args()
 
 
@@ -59,16 +62,31 @@ def remove_service(id):
             print(e)
 
 
-def check_health(processes):
+def remove_service_by_instance(instance):
+    for service in instance.services().data:
+        try:
+            client.action(service, 'remove')
+            print('####remove service: %s####' % instance.id)
+        except Exception, e:
+            print(e)
+
+
+def check_health(processes, resource_type):
     dt = datetime.datetime.utcnow()
     current_ts = int(time.mktime(dt.timetuple())*1000)
     print(processes.data)
     for ps in processes.data:
         timeout = current_ts - ps.startTimeTS
-        if timeout > args.timeout:
-            print('#####bad service: %s , id: %s####' % (ps.data.name,
-                                                         ps.resourceId))
-            remove_service(ps.resourceId)
+        if resource_type == 'service':
+            if timeout > args.service_timeout:
+                print('#####bad service: %s , id: %s####' % (ps.data.name,
+                                                             ps.resourceId))
+                remove_service(ps.resourceId)
+        if resource_type == 'instance':
+                instance = client.by_id_instance(ps.resourceId)
+                if instance.startCount > args.instance_start_count:
+                    print('#####bad instance id: %s####' % (ps.resourceId))
+                    remove_service_by_instance(instance)
 
 
 def launch():
@@ -77,13 +95,22 @@ def launch():
         client = gdapi.Client(url=args.cattle_url,
                               access_key=args.cattle_access_key,
                               secret_key=args.cattle_secret_key)
-    running_processes = client.list_processInstance(endTime_null=True,
+    running_services = client.list_processInstance(endTime_null=True,
+                                                   limit=100,
+                                                   sort=id,
+                                                   order='desc',
+                                                   processName='service.activate',
+                                                   exitReason='TIMEOUT')
+    check_health(running_services, 'service')
+
+    running_instances = client.list_processInstance(endTime_null=True,
                                                     limit=100,
                                                     sort=id,
                                                     order='desc',
-                                                    processName='service.activate',
-                                                    exitReason='TIMEOUT')
-    check_health(running_processes)
+                                                    processName='instance.start',
+                                                    exitReason='UNKNOWN_EXCEPTION')
+    check_health(running_instances, 'instance')
+
     threading.Timer(args.timer, launch).start()
 
 if __name__ == '__main__':
